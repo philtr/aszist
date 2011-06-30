33a40f90259a3b7ad8730024e84c26036df116e9
Phillip Ridlen <phillip@balcomagency.com>
2011-06-30 14:15:11 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aszist::Application.config.secret_token = '0b2b62153495d4c2c5ec9ad51fd97fe6215acc289b523551cc6b978374b9742d5882c3ccef98764fafd171d032e6a88d44c51c28610ce04db5db8bb784daffb1'
