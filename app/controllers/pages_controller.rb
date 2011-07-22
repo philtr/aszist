05ded28c763a3ef1eb8e3d808de18775e80c8ad9
Phillip Ridlen <phillip@balcomagency.com>
2011-07-22 15:34:37 -0500

    don't just render text for pages#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def index
     redirect_to(:controller => 'tickets', :action => 'index') and return if user_signed_in?
-    render :text => "Aszist"
   end
 end
