bdf0c39c18c31ff29945dd73e36f87f00716c603
Phillip Ridlen <phillip@balcomagency.com>
2011-07-27 10:59:36 -0500

    generate devise views and allow user to update their account information

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me
 
   ROLES = %w[customer agent admin]
 
