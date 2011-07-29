bc3d52dac8cd9f378aacf2e5048efdcc8d860656
Phillip Ridlen <phillip@balcomagency.com>
2011-07-29 15:36:25 -0500

    fix syntax error in abilities list

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     # Define abilities for the passed in user here.
     # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
     user ||= User.new
-    can :manage, Ticket do |ticket| :group => { :id => Ticket.where(:user_id => user.id).collect{ |t| t.id } }
+    can :manage, Ticket, :group => { :id => Ticket.where(:user_id => user.id).collect{ |t| t.id } }
     cannot :update_status, Ticket
     can [:create, :read, :update], User, :id => user.id
     if user.role? :agent
