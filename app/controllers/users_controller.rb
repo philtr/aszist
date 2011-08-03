e86a91fa780b0dccbf8545ea6169e4c063a74a99
Phillip Ridlen <phillip@balcomagency.com>
2011-08-03 10:51:43 -0500

    destroy user link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,4 +33,13 @@ class UsersController < ApplicationController
       end
     end
   end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :ok }
+    end
+  end
 end
