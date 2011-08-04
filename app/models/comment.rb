4cfcc29074b99e6c2924a83b50d73b9df9ea92b9
Phillip Ridlen <phillip@balcomagency.com>
2011-08-04 11:17:05 -0500

    strip out reply and signature

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,23 @@
 class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :user
+
+  before_create :strip_out_reply_and_signature
+
+private
+
+  def strip_out_reply_and_signature
+    delimiters = [
+      # Reply Delimiters
+      /^(On\s)?(.+?)\swrote:$/i,
+      /^-----Original Message-----$/i,
+      /^From:\s/i,
+      /^_+$/,
+      # Signature Delimiters
+      /^(Sincerely|Thanks|Regards|Cheers|Best),\n(.*?)\n/i,
+      /^-+$/,
+      /^Sent from my (iPhone|iPad|iPod(\stouch)?|Blackberry)(.*?)/i
+    ].each {|d| self.body = self.body.split(d).first }
+  end
+
 end
