621de8f7ea223fd36bae06a024801ccbe3f5768e
Phillip Ridlen <phillip@balcomagency.com>
2011-09-07 13:27:16 -0500

    fix ability for managing ticket

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,9 @@ class Ability
     # Define abilities for the passed in user here.
     # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
     user ||= User.new
-    can :manage, Ticket, :group => { :id => Ticket.where(:user_id => user.id).collect{ |t| t.id } }
+    can :manage, Ticket do |ticket|
+      ticket.agent_id == user.id or user.role? :admin
+    end
     cannot :update_status, Ticket
     can [:create, :read, :update], User, :id => user.id
     if user.role? :agent
