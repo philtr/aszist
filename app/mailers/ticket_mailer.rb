b1a2f21f3c3bee8c6d30675e360a94c4a01f6052
Phillip Ridlen <phillip@ridlen.net>
2011-10-25 22:46:27 -0500

    Remove mail_view
    Seemed to create more problems than it solved.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -17,47 +17,4 @@ class TicketMailer < ActionMailer::Base
     mail(:to => @to, :from => @from, :return_path => @reply_to,
       :subject => "[aszist] #{@ticket.subject}")
   end
-
-  class Preview < MailView
-    def agent_reply
-      bob = User.new(
-        :email => "bob@example.com",
-        :first_name => "Bob",
-        :last_name => "Jones")
-      steve = User.new(
-          :email => "steve@example.com",
-          :first_name => "Steve",
-          :last_name => "Saint",
-          :role => 1)
-      ticket = Ticket.new(
-        :subject => "Example issue title thingy",
-        :body => "Help! I'm having trouble figuring this crap out.\n\nThanks, Bob")
-      ticket.user = bob
-      ticket.agent = steve
-      comment = Comment.new(
-        :body => "Ok, let's figure this out together. Can you describe your problem for me?\n\nSteve",
-        :sent_to_user => true)
-      comment.ticket = ticket
-      comment.user = steve
-      ::TicketMailer.agent_reply(comment)
-    end
-
-    def notify_agent
-      bob = User.new(
-        :email => "bob@example.com",
-        :first_name => "Bob",
-        :last_name => "Jones")
-      steve = User.new(
-          :email => "steve@example.com",
-          :first_name => "Steve",
-          :last_name => "Saint",
-          :role => 1)
-      ticket = Ticket.new(
-        :subject => "Example issue title thingy",
-        :body => "Help! I'm having trouble figuring this crap out.\n\nThanks, Bob")
-      ticket.user = bob
-      ticket.agent = steve
-      ::TicketMailer.notify_agent(ticket)
-    end
-  end
 end
