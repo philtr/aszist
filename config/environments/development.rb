8f45afcdd0f501a1ea4b7201ea4b0f005c378394
Phillip Ridlen <phillip@ovenbits.com>
2013-07-27 13:20:13 -0500

    Remove config.whiny_nils and set config.eager_load

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,12 +6,11 @@ Aszist::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  config.eager_load = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
