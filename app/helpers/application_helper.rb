2bd5fdb43220eeb37c03155949f5fd272e7c2c91
Phillip Ridlen <phillip@ovenbits.com>
2014-02-15 19:51:51 -0600

    Building on Foundation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,19 @@
 module ApplicationHelper
   include Redcarpet
+
+  def flashy(flash)
+    flash.map do |type, message|
+      render "shared/flash/#{ type }", message: message
+    end.join.html_safe
+  end
+
+  def logo(link_url = root_path)
+    render "shared/logo", url: link_url
+  end
+
   def markdownify(text)
     markdown = Markdown.new(Render::XHTML.new(:hard_wrap => true))
     raw Render::SmartyPants.render(markdown.render(text))
   end
+
 end
